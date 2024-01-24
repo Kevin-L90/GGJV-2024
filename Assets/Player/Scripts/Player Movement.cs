652b10ef56ac8c0a764e54d6c8d7db62778e617c
UBC Student <kevinliu2002@yahoo.ca>
2024-01-23 21:14:40 -0800

    Player and Camera Rotation

--- Assets/Player/Scripts/Player Movement.cs
+++ Assets/Player/Scripts/Player Movement.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public GameObject player;
-
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position;
 
         if (Input.GetKey(KeyCode.A)) {
             transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * 100, Space.World);
